b24ab5ec79e54033c46ede247278d0dca9d0398f
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2017-11-12 14:39:33 +0100

    Versioning using SemVer 1.0.0 * Version will be set in script now * Changed major PowerShell Version to 5

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -4,12 +4,11 @@
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyProduct("MaRVIN Platform")]
-[assembly: AssemblyCompany("PHOENIX CONTACT GmbH &amp; Co. KG")]
-[assembly: AssemblyCopyright("Copyright © PHOENIX CONTACT GmbH & Co. KG 2017")]
+[assembly: AssemblyCompany("PHOENIX CONTACT")]
+[assembly: AssemblyCopyright("Copyright © PHOENIX CONTACT 2017")]
 [assembly: AssemblyTrademark("")]
 
 [assembly: AssemblyConfiguration("Debug")]
-
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: AssemblyInformationalVersion("3.0.0.0")]
