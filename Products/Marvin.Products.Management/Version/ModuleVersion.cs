0752abfb30d1b99dd49ae520f68b32180e9a732c
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2018-01-22 14:54:31 +0100

    Removed SVN version from Assemblies

--- Products/Marvin.Products.Management/Version/ModuleVersion.cs
+++ Products/Marvin.Products.Management/Version/ModuleVersion.cs
@@ -2,7 +2,7 @@
 {
     internal static class ModuleVersion
     {
-        public const string Version = "1.0.0.31112";
+        public const string Version = "3.0.0.0";
 		public const string RevisionDate = "2018/01/22 12:57:25";
     }
 }
