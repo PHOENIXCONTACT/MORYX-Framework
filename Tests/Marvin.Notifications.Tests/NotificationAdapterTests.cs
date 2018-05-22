3fc01fb87788bb7b8464e092abdc1815d9fbb2ae
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2018-05-22 13:48:07 +0200

    Fixed tests of notification adapter

--- Tests/Marvin.Notifications.Tests/NotificationAdapterTests.cs
+++ Tests/Marvin.Notifications.Tests/NotificationAdapterTests.cs
@@ -176,7 +176,7 @@ namespace Marvin.Notifications.Tests
 
             // Act
 
-            ((INotificationSenderAdapter) _notificationAdapter).Sync(new INotification[] {});
+            ((INotificationSenderAdapter) _notificationAdapter).Sync();
 
             // Assert
             Assert.AreEqual(4, counter, "There should be four publish events. One for each pending notification");
@@ -198,7 +198,7 @@ namespace Marvin.Notifications.Tests
             ((INotificationSenderAdapter) _notificationAdapter).Acknowledged += delegate { counter += 1; };
 
             // Act
-            ((INotificationSenderAdapter) _notificationAdapter).Sync(new INotification[] {notifiaction1, notifiaction2});
+            ((INotificationSenderAdapter) _notificationAdapter).Sync();
 
             // Assert
             Assert.AreEqual(2, counter, "There should be two ackowledge events. One for each pending acknowledgement which should be synchronized with the Publisher");
@@ -218,42 +218,10 @@ namespace Marvin.Notifications.Tests
             ((INotificationSenderAdapter) _notificationAdapter).Published += delegate { counter += 1; };
 
             // Act
-            ((INotificationSenderAdapter) _notificationAdapter).Sync(new INotification[] {notifiaction1, notification2});
+            ((INotificationSenderAdapter) _notificationAdapter).Sync();
 
             // Assert
             Assert.AreEqual(0, counter, "There should be no publish events because everything should be up to date");
-        }
-
-        [Test(Description = "A notification should be acknowledged during the synchronization if the sender is unknown")]
-        public void AckowledgeNotificationIfSenderIsUnknown()
-        {
-            // Arrange
-            var notification = new Notification();
-            ((IManagedNotification) notification).Identifier = "Test";
-            ((IManagedNotification) notification).Sender = "Test";
-            int counter = 0;
-            ((INotificationSenderAdapter) _notificationAdapter).Acknowledged += delegate { counter += 1; };
-
-            // Act
-            ((INotificationSenderAdapter) _notificationAdapter).Sync(new INotification[] {notification});
-
-            // Assert
-            Assert.AreEqual(1, counter, "There should be one acknowledgement if the sender is unknown");
-        }
-
-        [Test(Description = "An already published notification for a known sender should be stored in the adapter for a later ackowledgement")]
-        public void AddAPublishedNotificationForAKnownSender()
-        {
-            // Arrange
-            var notification = new Notification();
-            ((IManagedNotification) notification).Identifier = "Test";
-            ((IManagedNotification) notification).Sender = _notificationSenderMock.Object.Identifier;
-
-            // Act
-            ((INotificationSenderAdapter) _notificationAdapter).Sync(new INotification[] {notification});
-
-            // Assert
-            Assert.AreEqual(1, _notificationpublisher.GetPublished().Count, "There should be one published notification" );
         }
 
         [Test(Description = "Check that acknowledging a notification by the SenderAdapter-interface for an unknown notification throws an exception.")]
