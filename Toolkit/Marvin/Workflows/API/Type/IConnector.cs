fc59b8625f2d06b18198347f659b5578a4e415a0
Thomas Fuchs <tfuchs@phoenixcontact.com>
2018-05-28 10:12:35 +0200

    Monitor transitions in path predictor

--- Toolkit/Marvin/Workflows/API/Type/IConnector.cs
+++ Toolkit/Marvin/Workflows/API/Type/IConnector.cs
@@ -3,18 +3,8 @@
     /// <summary>
     /// Connector of one or more transistions
     /// </summary>
-    public interface IConnector
+    public interface IConnector : IWorkplanNode
     {
-        /// <summary>
-        /// Workplan unique element id of this connector
-        /// </summary>
-        long Id { get; set; }
-
-        /// <summary>
-        /// Optional name for the place
-        /// </summary>
-        string Name { get; set; }
-
         /// <summary>
         /// Classification of this connector
         /// </summary>
---
+++ Toolkit/Marvin/Workflows/API/Type/IWorkplanNode.cs
@@ -0,0 +1,18 @@
+﻿namespace Marvin.Workflows
+{
+    /// <summary>
+    /// Common interface for <see cref="IWorkplanStep"/> and <see cref="IConnector"/>
+    /// </summary>
+    public interface IWorkplanNode
+    {
+        /// <summary>
+        /// Workplan unique element id of this connector
+        /// </summary>
+        long Id { get; set; }
+
+        /// <summary>
+        /// Transition name
+        /// </summary>
+        string Name { get; }
+    }
+}
