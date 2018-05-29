e68e5024aa9d4b4cc0e667828473ea7d8ec0965a
Manuel Markwort <mma@msdmarkwort.de>
2018-05-29 15:40:35 +0200

    Using now assembly information of the executing assembly

--- Runtime/Marvin.Runtime.Console/Properties/AssemblyInfo.cs
+++ Runtime/Marvin.Runtime.Console/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Marvin.Runtime.Runner")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Heart of Gold - MarvinPlatform test application")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
