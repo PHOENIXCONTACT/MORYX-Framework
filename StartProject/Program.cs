48bbf53a7d13c72000f94c1e24515e6aabc87951
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2018-06-08 09:04:08 +0200

    Added assembly and package for AbstractionLayer TestTools

--- StartProject/Program.cs
+++ StartProject/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using Marvin.Runtime.Kernel;
+﻿using Marvin.Runtime.Kernel;
 
 namespace StartProject
 {
