79096d5e3c2545e43e450197164e485ff3ce8798
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2018-06-19 18:04:00 +0200

    Added DummyArticle and DummyProduct to TestTools

---
+++ src/Marvin.AbstractionLayer.TestTools/DummyArticle.cs
@@ -0,0 +1,11 @@
+﻿namespace Marvin.AbstractionLayer.TestTools
+{
+    /// <summary>
+    /// Dummy implementation of an article. Created by the <see cref="DummyProduct"/>
+    /// </summary>
+    public class DummyArticle : Article
+    {
+        /// <inheritdoc />
+        public override string Type => nameof(DummyArticle);
+    }
+}
--- src/Marvin.AbstractionLayer.TestTools/ReferenceCollection.cs
+++ src/Marvin.AbstractionLayer.TestTools/ReferenceCollection.cs
@@ -3,6 +3,9 @@ using Marvin.AbstractionLayer.Resources;
 
 namespace Marvin.AbstractionLayer.TestTools
 {
+    /// <summary>
+    /// Dummy implementation of a resource reference collection
+    /// </summary>
     public class ReferenceCollection<T> : List<T>, IReferences<T> where T : IResource
     {
 
