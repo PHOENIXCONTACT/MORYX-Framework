05845a70fdfc7bb995edf6c6adb42c1e898b5687
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2018-10-05 15:05:54 +0200

    Added SemiNumericComparer to order string represented integers by its value

---
+++ src/Marvin/Collections/SemiNumericComparer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Marvin.Collections
+{
+    /// <summary>
+    /// Comparer to order string represented integers by their number (int or string possible)
+    /// </summary>
+    public class SemiNumericComparer : IComparer<string>
+    {
+        // Origin: https://stackoverflow.com/questions/6396378/c-sharp-linq-orderby-numbers-that-are-string-and-you-cannot-convert-them-to-int
+        /// <inheritdoc />
+        public int Compare(string string1, string string2)
+        {
+            int value1;
+            int value2;
+            var string1IsNumeric = IsNumeric(string1, out value1);
+            var string2IsNumeric = IsNumeric(string2, out value2);
+
+            if (string1IsNumeric && string2IsNumeric)
+            {
+                if (value1 > value2) return 1;
+                if (value1 < value2) return -1;
+                if (value1 == value2) return 0;
+            }
+
+            if (string1IsNumeric && !string2IsNumeric)
+                return -1;
+
+            if (!string1IsNumeric && string2IsNumeric)
+                return 1;
+
+            return string.Compare(string1, string2, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Checks if the value is an integer
+        /// </summary>
+        public static bool IsNumeric(object value, out int i)
+        {
+            try
+            {
+                i = Convert.ToInt32(value.ToString());
+                return true;
+            }
+            catch (FormatException)
+            {
+                i = 0;
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Tests/Marvin.Tests/Collections/SemiNumericComparerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Marvin.Collections;
+using NUnit.Framework;
+
+namespace Marvin.Tests.Collections
+{
+    [TestFixture]
+    public class SemiNumericComparerTests
+    {
+        [Test(Description = "Tests if the comparer orders the string values numerically.")]
+        public void OrderStringsByIntValue()
+        {
+            // Arrange
+            var stringNumbers = new[] { "105", "101", "102", "103", "90" };
+
+            // Act
+            var ordered = stringNumbers.OrderBy(s => s, new SemiNumericComparer()).ToArray();
+
+            // Assert
+            Assert.AreEqual("90", ordered[0]);
+            Assert.AreEqual("101", ordered[1]);
+            Assert.AreEqual("102", ordered[2]);
+            Assert.AreEqual("103", ordered[3]);
+            Assert.AreEqual("105", ordered[4]);
+        }
+
+        [Test(Description = "Tests if the comparer orders the string values first numerically and then by string")]
+        public void OrderStringsByIntAndThenByString()
+        {
+            // Arrange
+            var stringNumbers = new[] { "105", "101", "Hundi", "Wau", "Wau", "90" };
+
+            // Act
+            var ordered = stringNumbers.OrderBy(s => s, new SemiNumericComparer()).ToArray();
+
+            // Assert
+            Assert.AreEqual("90", ordered[0]);
+            Assert.AreEqual("101", ordered[1]);
+            Assert.AreEqual("105", ordered[2]);
+            Assert.AreEqual("Hundi", ordered[3]);
+            Assert.AreEqual("Wau", ordered[4]);
+            Assert.AreEqual("Wau", ordered[5]);
+        }
+    }
+}
