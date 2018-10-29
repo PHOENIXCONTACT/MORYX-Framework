d02e41e9d820474a0ddb475c06951cddbbf65a40
Manuel Markwort <mma@msdmarkwort.de>
2018-10-29 12:29:48 +0000

    Added support for ScheduledExecution in NotSoParallelOps

--- src/Marvin.TestTools.UnitTest/NotSoParallelOps.cs
+++ src/Marvin.TestTools.UnitTest/NotSoParallelOps.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using Marvin.Threading;
 
 namespace Marvin.TestTools.UnitTest
 {
+    internal class TimerInfo
+    {
+        public int TimerId { get; set; }
+        public Timer Timer { get; set; }
+        public Exception Exception { get; set; }
+    }
+
     /// <summary>
     /// Replacement for parallel operations to achive synchronus behaviour in unit tests.
     /// </summary>
@@ -68,56 +78,105 @@ namespace Marvin.TestTools.UnitTest
         {
         }
 
-        /// <summary>
-        /// Execute operation periodically but non-stacking
-        /// </summary>
-        /// <returns>
-        /// Timer id to stop periodic execution
-        /// </returns>
+        private int _lastTimerId;
+        private readonly CountdownEvent _countdown = new CountdownEvent(1);
+        private readonly List<TimerInfo> _timers = new List<TimerInfo>();
+
+        /// <inheritdoc />
         public int ScheduleExecution(Action operation, int delayMs, int periodMs)
         {
-            throw new NotImplementedException($"{nameof(ScheduleExecution)} is not supported by {nameof(NotSoParallelOps)}");
+            return ScheduleExecution(state => operation(), new object(), delayMs, periodMs, false);
         }
 
-        /// <summary>
-        /// Execute operation periodically but non-stacking
-        /// </summary>
-        /// <returns>
-        /// Timer id to stop periodic execution
-        /// </returns>
+        /// <inheritdoc />
         public int ScheduleExecution(Action operation, int delayMs, int periodMs, bool criticalOperation)
         {
-            throw new NotImplementedException($"{nameof(ScheduleExecution)} is not supported by {nameof(NotSoParallelOps)}");
+            return ScheduleExecution(state => operation(), new object(), delayMs, periodMs, criticalOperation);
         }
 
-        /// <summary>
-        /// Execute operation periodically but non-stacking
-        /// </summary>
-        /// <returns>
-        /// Timer id to stop periodic execution
-        /// </returns>
+        /// <inheritdoc />
         public int ScheduleExecution<T>(Action<T> operation, T userState, int delayMs, int periodMs) where T : class
         {
-            throw new NotImplementedException($"{nameof(ScheduleExecution)} is not supported by {nameof(NotSoParallelOps)}");
+            return ScheduleExecution(operation, userState, delayMs, periodMs, false);
         }
 
-        /// <summary>
-        /// Execute operation periodically but non-stacking
-        /// </summary>
-        /// <returns>
-        /// Timer id to stop periodic execution
-        /// </returns>
+        /// <inheritdoc />
         public int ScheduleExecution<T>(Action<T> operation, T userState, int delayMs, int periodMs, bool criticalOperation) where T : class
         {
-            throw new NotImplementedException($"{nameof(ScheduleExecution)} is not supported by {nameof(NotSoParallelOps)}");
+            _countdown.AddCount();
+
+            var id = _lastTimerId++;
+            var timerInfo = new TimerInfo
+            {
+                TimerId = id
+            };
+
+            var timer = new Timer(new NonStackingTimerCallback(state =>
+            {
+                try
+                {
+                    operation(userState);
+                }
+                catch (Exception e)
+                {
+                    timerInfo.Exception = e;
+                }
+                finally
+                {
+                    if (periodMs <= 0)
+                    {
+                        StopExecution(id);
+                    }
+                }
+
+            }), userState, delayMs, periodMs);
+
+            timerInfo.Timer = timer;
+
+            lock (_timers)
+            {
+                _timers.Add(timerInfo);
+                return id;
+            }
+        }
+
+        /// <inheritdoc />
+        public void StopExecution(int timerId)
+        {
+            lock (_timers)
+            {
+                var timer = _timers.FirstOrDefault(t => t.TimerId == timerId);
+                if (timer != null)
+                {
+                    _countdown.Signal();
+                    timer.Timer.Dispose();
+                }
+            }
         }
 
         /// <summary>
-        /// Stop the execution of the timer with this id
+        /// Waits until all scheduled executions has been finished. Note that all execution routines has to be called already.
         /// </summary>
-        /// <param name="timerId"/>
-        public void StopExecution(int timerId)
+        /// <returns>Returns true if all timers has been finished or false if not all timers has been finished when timeout is over</returns>
+        public bool WaitForScheduledExecution(int timeoutMs)
         {
+            lock (this)
+            {
+                _countdown.Signal();
+                return _countdown.Wait(timeoutMs);
+            }
+        }
+
+        /// <summary>
+        /// Returns all exceptions that have been occurred within a scheduled execution operation
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Exception> ScheduledExecutionExceptions()
+        {
+            lock (this)
+            {
+                return _timers.Where(t => t.Exception != null).Select(t => t.Exception);
+            }
         }
 
         /// <summary>
@@ -125,6 +184,21 @@ namespace Marvin.TestTools.UnitTest
         /// </summary>
         public void Dispose()
         {
+            // Dispose all timers
+            foreach (var runningTimer in _timers)
+            {
+                runningTimer.Timer.Dispose();
+            }
+
+            lock (this)
+            {
+                if (_countdown.CurrentCount > 1)
+                {
+                    _countdown.Signal(_countdown.CurrentCount - 1);
+                }
+            }
+
+            _timers.Clear();
         }
     }
 }
