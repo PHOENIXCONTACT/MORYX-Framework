bea84a054949ee1994e92f0d10202e28baa05324
Manuel Markwort <mma@msdmarkwort.de>
2019-01-29 16:14:03 +0100

    Fixed how HeartOfGold SmokeTest is started from ps script

--- .build/BuildToolkit.ps1
+++ .build/BuildToolkit.ps1
@@ -229,7 +229,7 @@ function Invoke-SmokeTest([string]$RuntimePath, [int]$ModulesCount, [int]$Interr
     $randomIncrement = Get-Random -Minimum 2000 -Maximum 2100
     Write-Step "Invoking Runtime SmokeTest Modules: $ModulesCount, Interrupt Time: $InterruptTime, Port Increment: $randomIncrement."  
 
-    & $RuntimePath "-r=SmokeTest -e=$ModulesCount -i=$InterruptTime -pi=$randomIncrement"
+    & "$RuntimePath" @("-r=SmokeTest", "-e=$ModulesCount", "-i=$InterruptTime", "-pi=$randomIncrement")
     Invoke-ExitCodeCheck $LastExitCode;
 }
 
