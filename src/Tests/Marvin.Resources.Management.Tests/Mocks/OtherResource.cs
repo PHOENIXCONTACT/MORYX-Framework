f57bf14ab46e5b3efa4492ee4148d6cda05422fd
Thomas Fuchs <tfuchs@phoenixcontact.com>
2019-02-25 10:49:57 +0100

    Linker test for interference in save references

---
+++ src/Tests/Marvin.Resources.Management.Tests/Mocks/OtherResource.cs
@@ -0,0 +1,12 @@
+﻿using Marvin.AbstractionLayer.Resources;
+
+namespace Marvin.Resources.Management.Tests
+{
+    public class OtherResource : Resource
+    {
+    }
+
+    public class DifferentResource : Resource
+    {
+    }
+}
