5bdb7a879408d6a73d0d43095a6bca2fc69a09d9
Thomas Fuchs <tfuchs@phoenixcontact.com>
2019-03-08 14:53:04 +0100

    GetReferences in ReflectionTool

---
+++ src/Tests/Marvin.Tests/Extensions/AppDomain/ReferenceClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Marvin.Tests.Extensions
+{
+    internal class ReferenceClass
+    {
+        public int Ignore { get; set; }
+
+        public string NotRelevant { get; set; }
+
+        public BaseClass BaseRef1 { get; set; }
+
+        public ChildClass2 ChildRef { get; set; }
+
+        public List<ChildClass1> Children1 { get; set; }
+
+        public IEnumerable<ChildClass2> Children2 { get; set; }
+
+        public GranChildClass1[] EmptyArray { get; set; }
+
+        public GranChildClass2[] NullArray { get; set; }
+    }
+}
