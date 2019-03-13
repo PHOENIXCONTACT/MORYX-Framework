7666db7f8c3f52375923e4f8a379ceab53549c28
Thomas Fuchs <tfuchs@phoenixcontact.com>
2019-03-13 13:07:41 +0100

    Replace Priority enum with struct

---
+++ src/Tests/Marvin.Tests/Modules/PriorityTests.cs
@@ -0,0 +1,35 @@
+﻿using Marvin.Modules;
+using NUnit.Framework;
+
+namespace Marvin.Tests.Modules
+{
+    [TestFixture]
+    public class PriorityTests
+    {
+        [Test]
+        public void PriorityCompare()
+        {
+            // Arrange
+            var pMax = Priority.Max;
+            var pMin = Priority.Min;
+            var p0 = Priority.P0;
+            var p3 = Priority.P3;
+            var pCustom = Priority.P(42);
+
+            // Act
+
+            // Assert
+            Assert.Greater(pMax.CompareTo(pMin), 0);
+            Assert.AreEqual(pMax.CompareTo(p0), 0);
+            Assert.Less(pMin.CompareTo(p3), 0);
+            Assert.LessOrEqual(pCustom.CompareTo(p3), pCustom.CompareTo(p3));
+            Assert.IsTrue(p0 > p3);
+            Assert.IsTrue(p0 >= p3);
+            Assert.IsTrue(pMax == p0);
+            Assert.IsTrue(pMax <= p0);
+            Assert.IsTrue(pMax >= p0);
+            Assert.IsTrue(pCustom < p3);
+            Assert.IsTrue(pCustom <= p3);
+        }
+    }
+}
