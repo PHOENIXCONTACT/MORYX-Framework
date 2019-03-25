229d5ab969188778a9db295eff615f3c6071fe22
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2019-03-25 11:58:40 +0100

    Cleaned events of WcfClientFactory and tests for localized entry convert

--- src/Tests/Marvin.Tests/Serialization/DummyClass.cs
+++ src/Tests/Marvin.Tests/Serialization/DummyClass.cs
@@ -1,9 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using Marvin.Tests.Properties;
+using Marvin.Tests.Serialization;
 
 namespace Marvin.Tests
 {
+    [ClassDisplay(Name = nameof(strings.ClassName), ResourceType = typeof(strings))]
+    public class LocalizedClass
+    {
+        public const string PropDisplayNameAttributeDisplayName = "Display Name";
+
+        [Display(Name = nameof(strings.PropDisplayAttribute_Name), ResourceType = typeof(strings))]
+        public string PropDisplayAttribute { get; set; }
+
+        [DisplayName(PropDisplayNameAttributeDisplayName)]
+        public string PropDisplayNameAttribute { get; set; }
+    }
+
+    public class LocalizedMethodDummyClass
+    {
+        [Display(Name = nameof(strings.InitiateWorldTermination), Description = nameof(strings.InitiateWorldTerminationDescription), ResourceType = typeof(strings))]
+        public bool InitiateWorldTermination([Display(Name = nameof(strings.EvacuatePeopleParam), Description = nameof(strings.EvacuatePeopleParamDescription), ResourceType = typeof(strings))] bool evacuatePeople,
+            [Display(Name = nameof(strings.NameOfTerminatorParam), ResourceType = typeof(strings))] string nameOfTerminator)
+        {
+            return true;
+        }
+    }
+
     [ClassDisplay(Name = nameof(strings.CrashTestDummy), ResourceType = typeof(strings))]
     public class DummyClass
     {
