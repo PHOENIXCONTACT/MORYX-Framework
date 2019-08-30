6355ede45638b5b4629877f53b2384421473262e
Thomas Fuchs <tfuchs@phoenixcontact.com>
2019-08-30 22:34:40 +0200

    Offer default prototype importer

---
+++ src/Marvin.Products.Management/Plugins/Importers/DefaultImporter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Marvin.AbstractionLayer;
+using Marvin.Container;
+using Marvin.Modules;
+using Marvin.Serialization;
+using Marvin.Tools;
+
+namespace Marvin.Products.Management.Importers
+{
+    [ExpectedConfig(typeof(DefaultImporterConfig))]
+    [Plugin(LifeCycle.Transient, typeof(IProductImporter), Name = nameof(DefaultImporter))]
+    public class DefaultImporter : ProductImporterBase<DefaultImporterConfig, DefaultImporterParameters>
+    {
+        protected override IProduct[] Import(DefaultImporterParameters parameters)
+        {
+            // TODO: Use type wrapper
+            var type = ReflectionTool.GetPublicClasses<Product>(p => p.Name == parameters.ProductType)
+                .First();
+
+            var productType = (Product)Activator.CreateInstance(type);
+            productType.Identity = new ProductIdentity(parameters.Identifier, parameters.Revision);
+            productType.Name = parameters.Name;
+
+            return new IProduct[] { productType };
+        }
+    }
+
+    public class DefaultImporterParameters : PrototypeParameters
+    {
+        [Required, PossibleTypes(typeof(Product))]
+        public string ProductType { get; set; }
+    }
+}
---
+++ src/Marvin.Products.Management/Plugins/Importers/DefaultImporterConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Marvin.Products.Management.Importers
+{
+    public class DefaultImporterConfig : ProductImporterConfig
+    {
+        public override string PluginName => nameof(DefaultImporter);
+    }
+}
