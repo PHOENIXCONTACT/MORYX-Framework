985ec2fc5cf5a4f648cb55378880599fb8ab48bb
Thomas Fuchs <tfuchs@phoenixcontact.com>
2019-11-11 17:08:26 +0100

    Add pickbylight driver interface to AL

---
+++ src/AbstractionLayer/Drivers/PickByLight/LightInstructions.cs
@@ -0,0 +1,33 @@
+﻿namespace Marvin.AbstractionLayer.Drivers.PickByLight
+{
+    /// <summary>
+    /// Output structure for a pick by light driver
+    /// </summary>
+    public struct LightInstructions
+    {
+        /// <summary>
+        /// Activate light instructions
+        /// </summary>
+        public LightInstructions(long colorCode, string instruction)
+        {
+            Active = true;
+            ColorCode = colorCode;
+            Instruction = instruction;
+        }
+
+        /// <summary>
+        /// Instruction active
+        /// </summary>
+        public bool Active { get; }
+
+        /// <summary>
+        /// Encoded color 
+        /// </summary>
+        public long ColorCode { get; }
+
+        /// <summary>
+        /// Instruction text
+        /// </summary>
+        public string Instruction { get; set; }
+    }
+}
