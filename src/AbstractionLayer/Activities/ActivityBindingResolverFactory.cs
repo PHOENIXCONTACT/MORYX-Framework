934bbde9f2265391255b4dd52fd952cb0141f70f
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2020-01-15 15:47:52 +0100

    Added Activity- and RecipeBindingResolverFactory

---
+++ src/AbstractionLayer/Activities/ActivityBindingResolverFactory.cs
@@ -0,0 +1,34 @@
+﻿using Marvin.Bindings;
+
+namespace Marvin.AbstractionLayer
+{
+    /// <summary>
+    /// Default factory that can create resolvers based on activities for
+    /// <see cref="IActivity"/>, <see cref="IProductType"/>, <see cref="ProductInstance"/>, <see cref="Process"/> and <see cref="IRecipe"/>.
+    /// It also creates a resolver for unknown keys.
+    /// </summary>
+    public class ActivityBindingResolverFactory : ProcessBindingResolverFactory
+    {
+        /// <inheritdoc />
+        protected override IBindingResolverChain CreateBaseResolver(string baseKey)
+        {
+            switch (baseKey)
+            {
+                case "Activity":
+                    return new NullResolver();
+                case "Tracing":
+                    return new DelegateResolver(source => ((IActivity)source).Tracing);
+                case "Process":
+                    return new DelegateResolver(source => ((IActivity)source).Process);
+                case "Recipe":
+                    return new DelegateResolver(source => ((IActivity)source).Process.Recipe);
+                case "ProductType":
+                    return new ProductResolver();
+                case "ProductInstance":
+                    return new DelegateResolver(source => (((IActivity)source).Process as ProductionProcess)?.ProductInstance);
+                default:
+                    return null;
+            }
+        }
+    }
+}
