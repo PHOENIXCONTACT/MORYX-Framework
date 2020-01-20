8ab68ea96f5af8382b7c7d681330ed0c5f79fb35
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2020-01-20 11:43:31 +0100

    Added interface for articles with production date

---
+++ src/AbstractionLayer/Products/IProductionDate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Marvin.AbstractionLayer
+{
+    /// <summary>
+    /// Interface for articles with production date
+    /// </summary>
+    public interface IProductionDate
+    {
+        /// <summary>
+        /// Date of production
+        /// </summary>
+        DateTime ProductionDate { get; set; }
+    }
+}
