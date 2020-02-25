126ece0e3c6973bfb47b2df6b6413615e2591dd2
Thomas Fuchs <tfuchs@phoenixcontact.com>
2020-02-25 13:34:14 +0100

    Assembly description

--- src/Marvin/Properties/AssemblyInfo.cs
+++ src/Marvin/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("The one and only MARVIN library")]
+[assembly: AssemblyDescription("Root namespace of the MARVIN ecosystem")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
