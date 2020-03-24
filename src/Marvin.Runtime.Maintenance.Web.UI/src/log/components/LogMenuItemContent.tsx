96b36aa0a7da9937f5642a09370803e2abdd9bb6
Thomas Fuchs <tfuchs@phoenixcontact.com>
2020-03-24 15:55:07 +0100

    Add header to missing abstract and sealed classes

--- src/Marvin.Runtime.Maintenance.Web.UI/src/log/components/LogMenuItemContent.tsx
+++ src/Marvin.Runtime.Maintenance.Web.UI/src/log/components/LogMenuItemContent.tsx
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2020, Phoenix Contact GmbH & Co. KG
+ * Licensed under the Apache License, Version 2.0
+*/
+
 const TraceImg = require("../images/Trace.png");
 const DebugImg = require("../images/Debug.png");
 const InfoImg = require("../images/Info.png");
