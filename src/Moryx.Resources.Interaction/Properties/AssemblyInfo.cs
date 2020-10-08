4aff0417a14352c866c0639a5455b58ab355acec
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2020-10-08 10:44:32 +0200

    Added net45, net standard 2.0 and net standard 2.1 compatibility

--- src/Moryx.Resources.Interaction/Properties/AssemblyInfo.cs
+++ src/Moryx.Resources.Interaction/Properties/AssemblyInfo.cs
@@ -1,22 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using Moryx.AbstractionLayer;
-using Moryx.AbstractionLayer.Resources;
+﻿using Moryx.AbstractionLayer;
 using Moryx.Modules;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Moryx.Resources.Interaction")]
-[assembly: AssemblyDescription("Provides the default implementation if the resource interaction")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("583cbd84-dd9f-4834-a89c-1625a05ee15d")]
 
 [assembly: Bundle(AbstractionLayerBundle.Name, AbstractionLayerBundle.Version)]
--- src/Moryx.Resources.Management/Properties/AssemblyInfo.cs
+++ src/Moryx.Resources.Management/Properties/AssemblyInfo.cs
@@ -1,38 +1,8 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 using Moryx.AbstractionLayer;
-using Moryx.AbstractionLayer.Resources;
 using Moryx.Modules;
 using Moryx.Resources.Management;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Moryx.Resources.Management")]
-[assembly: AssemblyDescription("Moryx Runtime Module: ResourceManager")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f14d282b-a0e8-46d7-92b0-a173be7ce3da")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-// Moryx attributes
 [assembly: Bundle(AbstractionLayerBundle.Name, AbstractionLayerBundle.Version)]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo(ResourceProxyBuilder.AssemblyName)]
