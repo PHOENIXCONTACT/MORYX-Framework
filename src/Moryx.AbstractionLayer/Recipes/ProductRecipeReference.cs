d7a3bdee0aa3d96d52910c14e0f000e46d713bb4
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2021-04-19 12:54:43 +0200

    Added reference class for IProductRecipe

---
+++ src/Moryx.AbstractionLayer/Recipes/ProductRecipeReference.cs
@@ -0,0 +1,26 @@
+// Copyright (c) 2021, Phoenix Contact GmbH & Co. KG
+// Licensed under the Apache License, Version 2.0
+
+using Moryx.AbstractionLayer.Products;
+
+namespace Moryx.AbstractionLayer.Recipes
+{
+    /// <summary>
+    /// Temporary product recipe which should be replaced by the product management
+    /// </summary>
+    public class ProductRecipeReference : RecipeReference, IProductRecipe
+    {
+        /// <inheritdoc />
+        public IProductType Product { get; set; }
+
+        /// <inheritdoc />
+        public IProductType Target => null;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="ProductRecipeReference"/>
+        /// </summary>
+        public ProductRecipeReference(long recipeId) : base(recipeId)
+        {
+        }
+    }
+}
