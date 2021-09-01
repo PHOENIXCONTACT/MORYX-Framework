e42f321390466f8c13a2fdb11ebc330a43352c47
Dennis Beuchler <dennis@beuchler.net>
2021-09-01 21:43:38 +0200

    Added migration test to Moryx.TestTools.Test.Model

--- src/Moryx.TestTools.Test.Model/TestModelContext.cs
+++ src/Moryx.TestTools.Test.Model/TestModelContext.cs
@@ -1,8 +1,11 @@
 // Copyright (c) 2020, Phoenix Contact GmbH & Co. KG
 // Licensed under the Apache License, Version 2.0
 
+using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Moryx.Model;
+using Moryx.Model.Annotations;
 using Moryx.Model.Attributes;
 using Moryx.Model.PostgreSQL;
 
@@ -25,9 +28,19 @@ namespace Moryx.TestTools.Test.Model
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                var connectionString = configuration.GetConnectionString("Moryx.TestTools.Test.Model");
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+
             optionsBuilder.UseLazyLoadingProxies();
         }
-
         public virtual DbSet<CarEntity> Cars { get; set; }
 
         public virtual DbSet<WheelEntity> Wheels { get; set; }
