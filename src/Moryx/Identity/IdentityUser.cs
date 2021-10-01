4ee91fb44a8e81db2f09fbef830686fd510cfa97
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2021-10-01 09:57:44 +0200

    Added authorization service api and tagged OperationAccess as obsolete

---
+++ src/Moryx/Identity/IdentityUser.cs
@@ -0,0 +1,28 @@
+﻿namespace Moryx.Identity
+{
+    /// <summary>
+    /// Model of a user
+    /// </summary>
+    public class IdentityUser
+    {
+        /// <summary>
+        /// Unique user name
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// First name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// User password
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
