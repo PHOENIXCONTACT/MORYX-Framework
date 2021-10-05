6e887e6c97a16bd32bd7753716ced6ca5008ca66
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2021-10-05 09:42:35 +0200

    Added authorization context and identity configuration

---
+++ src/Moryx/Identity/IAuthorizationContext.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2021, Phoenix Contact GmbH & Co. KG
+// Licensed under the Apache License, Version 2.0
+
+namespace Moryx.Identity
+{
+    /// <summary>
+    /// Context to provide access for a principal
+    /// </summary>
+    public interface IAuthorizationContext
+    {
+        /// <summary>
+        /// Checks access for the given resource and action on a principal
+        /// </summary>
+        void CheckAccess(string resource, string action);
+    }
+}
