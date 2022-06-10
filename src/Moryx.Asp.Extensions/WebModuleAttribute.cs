15d61d549b5e774704d8216bb39a8a674b05dada
Thomas Fuchs <tfuchs@phoenixcontact.com>
2022-06-10 08:23:07 +0200

    Move WebModule attribute for new web UIs

---
+++ src/Moryx.Asp.Extensions/WebModuleAttribute.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2022, Phoenix Contact GmbH & Co. KG
+// Licensed under the Apache License, Version 2.0
+
+namespace Moryx.Asp.Integration
+{
+    /// <summary>
+    /// Decorator for web modules
+    /// </summary>
+    public class WebModuleAttribute
+    {
+        /// <summary>
+        /// Unique route of this module
+        /// </summary>
+        public string Route { get; }
+
+        /// <summary>
+        /// Recognizable icon of this module
+        /// </summary>
+        public string Icon { get; }
+
+        /// <summary>
+        /// Export page as web module under given route
+        /// </summary>
+        public WebModuleAttribute(string route) : this(route, "favorite")
+        {
+            
+        }
+
+        /// <summary>
+        /// Export page as web module under given route and icon
+        /// </summary>
+        public WebModuleAttribute(string route, string icon)
+        {
+            Route = route;
+            Icon = icon;
+        }
+    }
+}
