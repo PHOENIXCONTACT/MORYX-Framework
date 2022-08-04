d0a8757f31b2f504781704ca70bd252f56ca5dcc
Milad Katebi <Q9IDFE@europe.phoenixcontact.com>
2022-08-04 16:04:39 +0200

    Added db endpoints

---
+++ src/Moryx.Runtime.Endpoints/Databases/Endpoint/Request/RestoreDatabaseRequest.cs
@@ -0,0 +1,23 @@
+// Copyright (c) 2020, Phoenix Contact GmbH & Co. KG
+// Licensed under the Apache License, Version 2.0
+
+using Moryx.Runtime.Endpoints.Databases.Endpoint.Models;
+
+namespace Moryx.Runtime.Endpoints.Databases.Endpoint.Request
+{
+    /// <summary>
+    /// Request to restore a database
+    /// </summary>
+    public class RestoreDatabaseRequest
+    {
+        /// <summary>
+        /// Configuration of the database
+        /// </summary>
+        public DatabaseConfigModel Config { get; set; }
+
+        /// <summary>
+        /// Name of the backup that shall be restored
+        /// </summary>
+        public string BackupFileName { get; set; }
+    }
+}
