86ffa62f7079948ee4699351df260a834db9d0ae
Wjatscheslaw Sujev <wjatscheslaw.sujev@phoenixcontact.com>
2022-11-18 12:53:54 +0100

    Removed physical server info support

--- src/Moryx.Runtime.Endpoints/Common/Endpoint/CommonController.cs
+++ src/Moryx.Runtime.Endpoints/Common/Endpoint/CommonController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Moryx.Runtime.Endpoints.Common.Endpoint.Response;
 using System;
-using System.Diagnostics;
 using System.Globalization;
 using System.Reflection;
 
@@ -48,36 +47,6 @@ namespace Moryx.Runtime.Endpoints.Common.Endpoint
                 MachineName = Environment.MachineName,
                 OSInformation = Environment.OSVersion.ToString(),
                 UpTime = Environment.TickCount
-            };
-        }
-
-        [HttpGet("info/system/load")]
-        public ActionResult<SystemLoadResponse> GetSystemLoad()
-        {
-            var physicalMemory = HostHelper.PhysicalMemory();
-            var freePhysicalMemory = HostHelper.FreePhysicalMemory();
-
-            return new SystemLoadResponse
-            {
-                CPULoad = HostHelper.ProcessorTimePercentage(),
-                SystemMemoryLoad = (double)(physicalMemory - freePhysicalMemory) / physicalMemory * 100.0
-            };
-        }
-
-        [HttpGet("info/application/load")]
-        public ActionResult<ApplicationLoadResponse> GetApplicationLoad()
-        {
-            var physicalMemory = HostHelper.PhysicalMemory();
-            var cpuLoad = 0UL;
-
-            if (!Debugger.IsAttached)
-                cpuLoad = HostHelper.ProcessorTimePercentage(Process.GetCurrentProcess().ProcessName);
-
-            return new ApplicationLoadResponse
-            {
-                CPULoad = cpuLoad,
-                SystemMemory = physicalMemory,
-                WorkingSet = Environment.WorkingSet
             };
         }
     }
