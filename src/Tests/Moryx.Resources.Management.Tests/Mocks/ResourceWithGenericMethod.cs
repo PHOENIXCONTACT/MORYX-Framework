60307a9c0b0bef9630636f2bd77fb869d821ccc1
Matho Camara <matho.camara@gmail.com>
2023-09-27 16:05:15 +0200

    Recreate proxy bug of message drivers

---
+++ src/Tests/Moryx.Resources.Management.Tests/Mocks/ResourceWithGenericMethod.cs
@@ -0,0 +1,28 @@
+﻿using Moryx.AbstractionLayer.Drivers;
+using Moryx.AbstractionLayer.Drivers.InOut;
+using Moryx.AbstractionLayer.Drivers.Message;
+using Moryx.AbstractionLayer.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moryx.Resources.Management.Tests
+{
+    public interface IGenericMethodCall<T> : IResource
+    {
+        /// <summary>
+        /// Get channel using specialized API
+        /// </summary>
+        IList<TChannel> GenericMethod<TChannel>(string identifier);
+    }
+
+    public class ResourceWithGenericMethod : Driver, IGenericMethodCall<object>
+    {
+        public IList<TChannel> GenericMethod<TChannel>(string identifier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
