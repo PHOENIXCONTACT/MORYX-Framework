0e84b209915fff7ae4e50a824393066739067f67
Christian Siewert <csiewert@phoenixcontact.com>
2024-04-22 15:21:38 +0200

    Use Material UI for `HealthStateBadge`

--- src/Moryx.CommandCenter.Web/src/dashboard/components/HealthStateBadge.tsx
+++ src/Moryx.CommandCenter.Web/src/dashboard/components/HealthStateBadge.tsx
@@ -3,8 +3,8 @@
  * Licensed under the Apache License, Version 2.0
 */
 
+import Chip from "@mui/material/Chip";
 import * as React from "react";
-import { Badge } from "reactstrap";
 import { ModuleServerModuleState } from "../../modules/models/ModuleServerModuleState";
 import { HealthStateToCssClassConverter } from "../converter/HealthStateToCssClassConverter";
 
@@ -19,8 +19,8 @@ export class HealthStateBadge extends React.Component<HealthStateBadgeProps> {
 
     public render(): React.ReactNode {
         const text = ModuleServerModuleState[this.props.HealthState];
-        const color = HealthStateToCssClassConverter.Convert(this.props.HealthState);
+        const color = HealthStateToCssClassConverter.Color(this.props.HealthState);
 
-        return (<Badge color={color.Background}>{text}</Badge>);
+        return (<Chip sx={{fontSize: "0.6rem", height: "1rem"}} color={color} size="small" label={text}/>);
     }
 }
