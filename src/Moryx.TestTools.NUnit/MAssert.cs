ca09a3d19043315c38e0178d181f84435a536c05
Andre Niggemann <aniggemann@phoenixcontact.com>
2024-07-11 11:14:56 +0200

    InitialDesign for MAssert and modified Transition Test as an example for the usage. Feel free to break the tests and switch between Assert.That and MAssert.That to see the effects.

---
+++ src/Moryx.TestTools.NUnit/MAssert.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.Internal;
+using System;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace Moryx.TestTools.NUnit
+{
+    public abstract class MAssert
+    {
+        public static void That(bool condition, string? message = null, [CallerArgumentExpression(nameof(condition))] string? predicateExpression = null)
+        {
+            That(condition, Is.True, message, predicateExpression);
+        }
+        public static void That(Func<bool> predicate, string? message = null, [CallerArgumentExpression(nameof(predicate))]string? predicateExpression = null)
+        {
+            That(predicate, Is.True, message, predicateExpression);
+        }
+
+        public static void That<T>(T actual, IResolveConstraint constraint, string? message = null, [CallerArgumentExpression(nameof(actual))] string? predicateExpression = null)
+        {
+            if (message != null)
+            {
+                message = $"{message}\nExpression: {predicateExpression}";
+            }
+            else
+            {
+                message = predicateExpression;
+            }
+            Assert.That(actual, constraint, message);
+        }
+
+        public static void That<T>(Func<T> actualExpression, Constraint constraint, string? message = null, [CallerArgumentExpression(nameof(actualExpression))] string? predicateExpression = null)
+        {
+            if (message != null)
+            {
+                message = $"{message}\nExpression: {predicateExpression}";
+            }
+            else
+            {
+                message = predicateExpression;
+            }
+            int fails = TestExecutionContext.CurrentContext.CurrentResult.PendingFailures;
+            T value = default(T)!; 
+            Assert.That<T>(() => value = actualExpression(), new ThrowsNothingConstraint(), $"{message}\nExpected {constraint.Description} and");
+            if (TestExecutionContext.CurrentContext.CurrentResult.PendingFailures > fails) return; // TODO: Check if we there could be multithreading issues and whether or not we care
+            Assert.That(value, constraint, message);
+        }
+    }
+
+}
