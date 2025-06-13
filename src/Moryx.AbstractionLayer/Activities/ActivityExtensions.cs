71ea8a6012afe6bb133f9e903cba1e7a1859fdea
Sascha Priefer <spriefer@phoenixcontact.com>
2025-06-13 14:16:57 +0200

    Add ProcessExtensions to get all NextActivities and all CurrentActivities. Add ActivityExtension to get the task of an activity.

---
+++ src/Moryx.AbstractionLayer/Activities/ActivityExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moryx.AbstractionLayer.Recipes;
+using Moryx.Workplans;
+
+namespace Moryx.AbstractionLayer
+{
+    /// <summary>
+    /// Extensions for <see cref="Activity"/>
+    /// </summary>
+    public static class ActivityExtensions
+    {
+        /// <summary>
+        /// Get the task of an activity
+        /// </summary>
+        public static IWorkplanStep GetTask(this Activity activity)
+        {
+            if (activity.Process.Recipe is WorkplanRecipe workplanRecipe)
+            {
+                var step = workplanRecipe.Workplan.Steps.FirstOrDefault(s => s.Id == activity.StepId);
+                return step;
+            }
+            return null;
+        }
+    }
+}
