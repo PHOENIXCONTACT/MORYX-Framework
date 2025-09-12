4e57e0245811a9072834805cc30ead3e31648631
André Niggemann <aniggemann@phoenixcontact.com>
2025-09-12 09:27:36 +0200

    Add comments for public API

--- src/Moryx.Runtime.Kernel/MoryxHost.cs
+++ src/Moryx.Runtime.Kernel/MoryxHost.cs
@@ -2,28 +2,45 @@
 using Microsoft.Extensions.Logging;
 using Moryx.Runtime.Modules;
 using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Moryx.Runtime.Kernel;
 
+
+/// <summary>
+/// A background service that handles starting and stopping the moryx modules.
+/// On windows, it also provides gracefull shutdown when the console window is closed.
+/// </summary>
 public class MoryxHost : BackgroundService
 {
     private readonly IModuleManager moduleManager;
     private readonly IHost lifeTime;
     private readonly ILogger<MoryxHost> logger;
 
-    // Console shutdown handling according to https://stackoverflow.com/questions/21751545/how-to-make-a-console-app-exit-gracefully-when-it-is-closed
+    /// <summary>
+    /// Console shutdown handling according to https://stackoverflow.com/questions/21751545/how-to-make-a-console-app-exit-gracefully-when-it-is-closed
+    /// </summary>
+    /// <param name="Handler"></param>
+    /// <param name="Add"></param>
+    /// <returns></returns>
     [DllImport("Kernel32")]
     public static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
 
-    // A delegate type to be used as the handler routine 
-    // for SetConsoleCtrlHandler.
+    /// <summary>
+    /// A delegate type to be used as the handler routine 
+    /// for SetConsoleCtrlHandler.
+    /// </summary>
+    /// <param name="CtrlType"></param>
+    /// <returns></returns>
     public delegate bool HandlerRoutine(CtrlTypes CtrlType);
 
-    // An enumerated type for the control messages
-    // sent to the handler routine.
+    /// <summary>
+    /// An enumerated type for the control messages
+    /// sent to the handler routine.
+    /// </summary>
     public enum CtrlTypes
     {
         CTRL_C_EVENT = 0,
@@ -51,6 +68,12 @@ public class MoryxHost : BackgroundService
         return true;
     }
 
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="moduleManager">The moryx module manager. Used to start the moryx system</param>
+    /// <param name="lifeTime">The application host. Used to stop the application when a console controle event is received</param>
+    /// <param name="logger">Logger for diagnostic messages</param>
     public MoryxHost(IModuleManager moduleManager, IHost lifeTime, ILogger<MoryxHost> logger)
     {
         this.moduleManager = moduleManager;
@@ -58,11 +81,18 @@ public class MoryxHost : BackgroundService
         this.logger = logger;
     }
 
-
+    /// <summary>
+    /// State of the MoryxHost 
+    /// </summary>
     public MoryxHostState State { get; private set; }
 
+    /// <summary>
+    /// Inform subscribers about state changes.
+    /// Probably most usefull to receive a notification before MORYX shuts down
+    /// </summary>
     public event EventHandler<MoryxHostState> StateChanged;
 
+    /// <inheritdoc/>
     public override async Task StartAsync(CancellationToken cancellationToken)
     {
         State = MoryxHostState.Starting;
@@ -79,17 +109,24 @@ public class MoryxHost : BackgroundService
         State = MoryxHostState.Started;
         StateChanged?.Invoke(this, State);
     }
+
+    /// <inheritdoc/>
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        // we don't have a long running task to perform
         return Task.CompletedTask;
     }
 
+    /// <inheritdoc/>
     public override async Task StopAsync(CancellationToken cancellationToken)
     {
         State = MoryxHostState.Stopping;
         StateChanged?.Invoke(this, State);
+
         moduleManager.StopModules();
+
         await base.StopAsync(cancellationToken);
+
         State = MoryxHostState.Stopped;
         StateChanged?.Invoke(this, State);
     }
