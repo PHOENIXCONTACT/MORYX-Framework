37f9b179f8fe7e89f38e6c92f05d72b6cb6618e9
Niklas Hamann <nhamann@phoenixcontact.com>
2025-10-13 11:18:52 +0200

    Add `.GetOrderNumber()` and `.GetOperationNumber()` to `IProcessExtensions`
    Also added tests.
    
    (cherry picked from commit d6d936e9de127951f875720580e256217990cf8d)

---
+++ src/Tests/Moryx.ControlSystem.Tests/Mocks/DummyRecipe.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2023, Phoenix Contact GmbH & Co. KG
+
+using Moryx.AbstractionLayer.Recipes;
+using Moryx.ControlSystem.Recipes;
+
+namespace Moryx.ControlSystem.Tests.Mocks
+{
+    public class DummyRecipe : ProductionRecipe, IOrderBasedRecipe
+    {
+        public string OrderNumber { get; set; }
+        public string OperationNumber { get; set; }
+
+        public DummyRecipe()
+        {
+        }
+    }
+}
