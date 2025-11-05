a436f448c8702a4acabf8f55e1af40d90d557ba5
André Niggemann <aniggemann@phoenixcontact.com>
2025-11-05 10:00:28 +0100

    Add missing using

--- src/Moryx.Drivers.Mqtt/States/ConnectingToBrokerState.cs
+++ src/Moryx.Drivers.Mqtt/States/ConnectingToBrokerState.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2025, Phoenix Contact GmbH & Co. KG
 // Licensed under the Apache License, Version 2.0
 
+using Microsoft.Extensions.Logging;
 using Moryx.AbstractionLayer.Drivers;
 
 namespace Moryx.Drivers.Mqtt.States
