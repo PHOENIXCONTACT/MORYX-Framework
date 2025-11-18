c5fcdafb73260aebc891f781ad8ac6af166360fa
jsonBackup <jason.becker@phoenixcontact.com[http]>
2025-11-18 14:43:49 +0100

    Regenerate `src/app/api` in `Moryx.Workplans.Web`

--- src/Moryx.Workplans.Web/src/app/api/models/product-file-model.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export interface ProductFileModel {
-  fileHash?: null | string;
-  fileName?: null | string;
-  filePath?: null | string;
-  mimeType?: null | string;
-  propertyName?: null | string;
-}
