9a33d0fb43e5a25fec5c2831a044b71493491e75
Marcel Vielhaus <mvielhaus@phoenixcontact.com>
2025-11-24 08:14:34 +0100

    Add missing migration causing exception after update
    EF Core 9 introduced a warning as error on pending migrations https://learn.microsoft.com/en-us/dotnet/core/compatibility/10.0

---
+++ src/Moryx.Resources.Management/Model/Migrations/Npgsql/20251124065017_UpdateEf10.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Moryx.Resources.Management.Model.Migrations.Npgsql
+{
+    /// <inheritdoc />
+    public partial class UpdateEf10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                schema: "public",
+                table: "Resources",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Deleted",
+                schema: "public",
+                table: "Resources",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                schema: "public",
+                table: "Resources",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                schema: "public",
+                table: "Resources",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Deleted",
+                schema: "public",
+                table: "Resources",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                schema: "public",
+                table: "Resources",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
