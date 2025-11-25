4d2f4e041f0d72902e92c36a1055ae2c978da6e3
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2025-11-25 10:36:58 +0100

    Added more common API of IPickByLightDriver

---
+++ src/Moryx.AbstractionLayer/Drivers/PickByLight/InstructionResult.cs
@@ -0,0 +1,12 @@
+// Copyright (c) 2025, Phoenix Contact GmbH & Co. KG
+// Licensed under the Apache License, Version 2.0
+
+namespace Moryx.AbstractionLayer.Drivers.PickByLight;
+
+/// <summary>
+/// Result of the instruction for the pick by light system
+/// </summary>
+public class InstructionResult
+{
+
+}
---
+++ src/Moryx.AbstractionLayer/Drivers/PickByLight/LightMode.cs
@@ -0,0 +1,35 @@
+// Copyright (c) 2025, Phoenix Contact GmbH & Co. KG
+// Licensed under the Apache License, Version 2.0
+
+namespace Moryx.AbstractionLayer.Drivers.PickByLight;
+
+/// <summary>
+/// Mode of the light of the pick-by-light system
+/// </summary>
+public enum LightMode
+{
+    /// <summary>
+    /// Light is turned off
+    /// </summary>
+    Off,
+
+    /// <summary>
+    /// Light is solid
+    /// </summary>
+    Solid,
+
+    /// <summary>
+    /// Light blinks slow
+    /// </summary>
+    BlinkSlow,
+
+    /// <summary>
+    /// Light blinks fast
+    /// </summary>
+    BlinkFast,
+
+    /// <summary>
+    /// All lights should be off
+    /// </summary>
+    AllOff
+}
