f097369f0b516a205d9533a1a376b13a23a14c48
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2025-12-02 10:35:21 +0100

    Fixed TextBindingResolver and VisualInsturctions.Tests

--- src/Tests/Moryx.Tests/Bindings/Dummies/Branch.cs
+++ src/Tests/Moryx.Tests/Bindings/Dummies/Branch.cs
@@ -1,10 +1,14 @@
 // Copyright (c) 2025, Phoenix Contact GmbH & Co. KG
 // Licensed under the Apache License, Version 2.0
 
+using System.Collections.Generic;
+
 namespace Moryx.Tests.Bindings
 {
     public class Branch
     {
         public string Name { get; set; }
+
+        public Dictionary<string, string> Dictionary { get; set; }
     }
 }
