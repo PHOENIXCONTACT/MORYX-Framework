2e147817cdea5d3f08a0cec46a898e96fd8c1ca2
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2025-12-03 14:19:05 +0100

    Added missing migrations for ProcessEntity

---
+++ src/Moryx.ControlSystem.ProcessEngine/Model/Migrations/Npgsql/20251202141815_RemovedTypeNameProcessEntity.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2025, Phoenix Contact GmbH & Co. KG
+// Licensed under the Apache License, Version 2.0
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Moryx.ControlSystem.ProcessEngine.Model.Migrations.Npgsql
+{
+    /// <inheritdoc />
+    public partial class RemovedTypeNameProcessEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TypeName",
+                schema: "public",
+                table: "Processes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TypeName",
+                schema: "public",
+                table: "Processes",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
