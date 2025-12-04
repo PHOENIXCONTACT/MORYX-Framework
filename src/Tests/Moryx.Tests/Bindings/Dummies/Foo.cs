f271e175ace019a30ca78e417bba4de23534697f
pbls31 <slimberg@phoenixcontact.com>
2025-12-04 12:47:54 +0100

    Added tests to ensure functionality

--- src/Tests/Moryx.Tests/Bindings/Dummies/Foo.cs
+++ src/Tests/Moryx.Tests/Bindings/Dummies/Foo.cs
@@ -6,5 +6,11 @@ namespace Moryx.Tests.Bindings
     public class Foo
     {
         public Branch Branch { get; set; }
+
+        public Branch Branch1 { get; set; }
+
+        public string Name { get; set; }
+
+        public int Value1 { get; set; }
     }
 }
