547fc8d7637245456be8782798f272a28a8fbf34
Marcel Vielhaus <mvielhaus@phoenixcontact.com>
2026-01-09 06:25:39 +0100

    Fix multiple isue in MORYX Access Management
    - While a migration for the IdentityDbContext was added in e59c1241cebb6924ef00d18357f4127f855814c8 to resolve the PendingModelChangesWarning exception, this migration did not correctly reflect the expected state of the snapshot. In accordance with the [EF Core Migration Guide](https://learn.microsoft.com/en-gb/ef/core/what-is-new/ef-core-9.0/breaking-changes#mitigations) we now added implementation to the `MoryxIdentitiesDbContextDesignTimeFactory` as described for using Asp.Net Core Identity. Based on htese changes the MORYX10 migration was replaced.
    - Updates migration notes
    - Updates the `DesignTimeDbContextFactory` to reuse the connection string parsing while creating the `IdentityDbContext` in the local design time factory
    - Added a gitignore and falsely removed content from the `wwwroot` directory of the AccessManagemen

--- src/Moryx.Identity.AccessManagement/Migrations/20260108201039_MORYX10.cs
+++ src/Moryx.Identity.AccessManagement/Migrations/20260108201039_MORYX10.cs
@@ -10,104 +10,81 @@ namespace Moryx.Identity.AccessManagement.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_RefreshTokens_AspNetUsers_UserId",
-                table: "RefreshTokens");
-
             migrationBuilder.AlterColumn<string>(
                 name: "Name",
                 table: "AspNetUserTokens",
-                type: "text",
+                type: "character varying(128)",
+                maxLength: 128,
                 nullable: false,
                 oldClrType: typeof(string),
-                oldType: "character varying(128)",
-                oldMaxLength: 128);
+                oldType: "text");
 
             migrationBuilder.AlterColumn<string>(
                 name: "LoginProvider",
                 table: "AspNetUserTokens",
-                type: "text",
+                type: "character varying(128)",
+                maxLength: 128,
                 nullable: false,
                 oldClrType: typeof(string),
-                oldType: "character varying(128)",
-                oldMaxLength: 128);
+                oldType: "text");
 
             migrationBuilder.AlterColumn<string>(
                 name: "ProviderKey",
                 table: "AspNetUserLogins",
-                type: "text",
+                type: "character varying(128)",
+                maxLength: 128,
                 nullable: false,
                 oldClrType: typeof(string),
-                oldType: "character varying(128)",
-                oldMaxLength: 128);
+                oldType: "text");
 
             migrationBuilder.AlterColumn<string>(
                 name: "LoginProvider",
                 table: "AspNetUserLogins",
-                type: "text",
+                type: "character varying(128)",
+                maxLength: 128,
                 nullable: false,
                 oldClrType: typeof(string),
-                oldType: "character varying(128)",
-                oldMaxLength: 128);
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_RefreshTokens_AspNetUsers_UserId",
-                table: "RefreshTokens",
-                column: "UserId",
-                principalTable: "AspNetUsers",
-                principalColumn: "Id");
+                oldType: "text");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_RefreshTokens_AspNetUsers_UserId",
-                table: "RefreshTokens");
-
             migrationBuilder.AlterColumn<string>(
                 name: "Name",
                 table: "AspNetUserTokens",
-                type: "character varying(128)",
-                maxLength: 128,
+                type: "text",
                 nullable: false,
                 oldClrType: typeof(string),
-                oldType: "text");
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
 
             migrationBuilder.AlterColumn<string>(
                 name: "LoginProvider",
                 table: "AspNetUserTokens",
-                type: "character varying(128)",
-                maxLength: 128,
+                type: "text",
                 nullable: false,
                 oldClrType: typeof(string),
-                oldType: "text");
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
 
             migrationBuilder.AlterColumn<string>(
                 name: "ProviderKey",
                 table: "AspNetUserLogins",
-                type: "character varying(128)",
-                maxLength: 128,
+                type: "text",
                 nullable: false,
                 oldClrType: typeof(string),
-                oldType: "text");
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
 
             migrationBuilder.AlterColumn<string>(
                 name: "LoginProvider",
                 table: "AspNetUserLogins",
-                type: "character varying(128)",
-                maxLength: 128,
+                type: "text",
                 nullable: false,
                 oldClrType: typeof(string),
-                oldType: "text");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_RefreshTokens_AspNetUsers_UserId",
-                table: "RefreshTokens",
-                column: "UserId",
-                principalTable: "AspNetUsers",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
         }
     }
 }
