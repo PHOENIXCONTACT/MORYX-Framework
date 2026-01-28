2b79b073af38204c3ec2f8fed354c50a935a9eec
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2026-01-28 19:23:54 +0100

    Migrated Operators to Angular and Material 21

--- src/Moryx.Operators.Web/app/src/main.ts
+++ src/Moryx.Operators.Web/app/src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from "@angular/core";
 /*
  * Copyright (c) 2026 Phoenix Contact GmbH & Co. KG
  * Licensed under the Apache License, Version 2.0
@@ -7,6 +8,6 @@ import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]})
   .catch(err => console.error(err));
 
