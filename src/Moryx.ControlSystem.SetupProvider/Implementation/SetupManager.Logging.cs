61dc4fc5e55193faa2da2e66eb4a335929c3c624
Robert Siemens <robert.siemens@phoenixcontact.com>
2026-01-29 10:44:50 +0100

    "Cleaned up logging logic by removing unnecessary LogLevel.Trace checks and unifying the detailed log messages.

--- src/Moryx.ControlSystem.SetupProvider/Implementation/SetupManager.Logging.cs
+++ src/Moryx.ControlSystem.SetupProvider/Implementation/SetupManager.Logging.cs
@@ -11,19 +11,11 @@ internal partial class SetupManager
         IEnumerable<ICell> targetCells,
         ILogger logger)
     {
-        if (logger.IsEnabled(LogLevel.Trace))
-        {
-            logger.LogTrace(
-                "Target already provides required capabilities {capabilities} (cells=[{cells}])",
-                change.TargetCapabilities,
-                string.Join(", ", targetCells.Select(c => c.Name)));
-        }
-        else
-        {
-            logger.LogTrace(
-                "Target already provides required capabilities {capabilities}",
-                change.TargetCapabilities);
-        }
+        logger.LogTrace(
+         "Target already provides required capabilities {capabilities} (cells=[{cells}])",
+         change.TargetCapabilities,
+         string.Join(", ", targetCells.Select(c => c.Name)));
+
         return true;
     }
 
@@ -32,20 +24,12 @@ internal partial class SetupManager
         IEnumerable<ICell> currentCells,
         ILogger logger)
     {
-        if (logger.IsEnabled(LogLevel.Trace))
-        {
-            logger.LogTrace(
-                "Current capabilities were already removed from target cell {capabilities} (cells=[{cells}])",
-                change.CurrentCapabilities,
-                string.Join(", ", currentCells.Select(c => c.Name)));
-        }
-        else
-        {
-            logger.LogTrace(
-                "Target does not provide current capabilities {capabilities}",
-                change.CurrentCapabilities);
-        }
+        logger.LogTrace(
+            "Current capabilities were already removed from target cell {capabilities} (cells=[{cells}])",
+            change.CurrentCapabilities,
+            string.Join(", ", currentCells.Select(c => c.Name)));
 
         return true;
     }
+
 }
