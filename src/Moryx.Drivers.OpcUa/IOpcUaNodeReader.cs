766095ad743a7759b8bbee1c941ef2a7f1f69e09
Christian Siewert <csiewert@phoenixcontact.com>
2026-06-22 15:51:03 +0200

    Fix minor issues

--- src/Moryx.Drivers.OpcUa/IOpcUaNodeReader.cs
+++ src/Moryx.Drivers.OpcUa/IOpcUaNodeReader.cs
@@ -12,10 +12,5 @@ internal interface IOpcUaNodeReader
     /// <summary>
     /// Reads node attributes from an OPC UA server
     /// </summary>
-    /// <param name="nodeId"></param>
-    /// <param name="namespaceTable"></param>
-    /// <param name="session"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
     public Task<OpcUaNode> ReadNodeAsync(string nodeId, NamespaceTable namespaceTable, ISession session, CancellationToken cancellationToken);
 }
