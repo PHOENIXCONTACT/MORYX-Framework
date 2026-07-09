342314717e5308ae5ad03411e7caed54d89e47ea
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2026-07-09 12:43:13 +0200

    Removed launchers wc-definition for notification-bar

--- src/Moryx.Launcher/app/src/main.ts
+++ src/Moryx.Launcher/app/src/main.ts
@@ -7,16 +7,12 @@ import { createApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { NotificationsBar } from './app/notifications-bar/notifications-bar';
 import { ModuleOverview } from './app/module-overview/module-overview';
 
 createApplication(appConfig)
   .then(appRef => {
     const LauncherElement = createCustomElement(App, {injector: appRef.injector});
     customElements.define('moryx-launcher', LauncherElement);
-
-    const NotificationBarElement = createCustomElement(NotificationsBar, {injector: appRef.injector});
-    customElements.define('moryx-notifications-bar', NotificationBarElement);
 
     const ModuleOverviewElement = createCustomElement(ModuleOverview, {injector: appRef.injector});
     customElements.define('moryx-module-overview', ModuleOverviewElement);
