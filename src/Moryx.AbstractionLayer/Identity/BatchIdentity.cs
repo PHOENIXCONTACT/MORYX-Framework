e8bbfa6830cd8fb766a146ab4e5284bde50e78b2
Marcel Vielhaus <mvielhaus@phoenixcontact.com>
2026-07-17 08:22:07 +0200

    Add display attributes to `BatchIdentity`

--- src/Moryx.AbstractionLayer/Identity/BatchIdentity.cs
+++ src/Moryx.AbstractionLayer/Identity/BatchIdentity.cs
@@ -1,16 +1,20 @@
 // Copyright (c) 2026 Phoenix Contact GmbH & Co. KG
 // Licensed under the Apache License, Version 2.0
 
+using System.ComponentModel.DataAnnotations;
 using Moryx.AbstractionLayer.Products;
+using Moryx.AbstractionLayer.Properties;
 
 namespace Moryx.AbstractionLayer.Identity;
 
 /// <summary>
 /// Identity to assign on <see cref="ProductInstance"/> to identify batches
 /// </summary>
+[Display(Name = nameof(Strings.BatchIdentity_Name), Description = nameof(Strings.BatchIdentity_Description), ResourceType = typeof(Strings))]
 public class BatchIdentity : IIdentity
 {
     /// <inheritdoc />
+    [Display(Name = nameof(Strings.BatchIdentity_Identifier_Name), Description = nameof(Strings.BatchIdentity_Identifier_Description), ResourceType = typeof(Strings))]
     public string Identifier { get; private set; }
 
     /// <summary>
