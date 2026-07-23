857d90c9670dcb3213d0c64ff7e94db280e26f2d
Marcel Vielhaus <mvielhaus@phoenixcontact.com>
2026-07-23 10:27:53 +0200

    Add Rest API for Material Management Endpoint
    Adds methods to
    - get all containers
    - get all container types
    - post a pre-advice
    - get a stream with container updates

---
+++ src/Moryx.Material.Endpoints/PreAdvideModel.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2026 Phoenix Contact GmbH & Co. KG
+// Licensed under the Apache License, Version 2.0
+
+using System.Runtime.Serialization;
+
+namespace Moryx.Material.Endpoints;
+
+[DataContract]
+public enum PreAdviceDepartureReasonModel
+{
+    [EnumMember]
+    FinishedGoods = 0,
+
+    [EnumMember]
+    UnusedMaterial = 1,
+
+    [EnumMember]
+    Transfer = 2,
+
+    [EnumMember]
+    Scrap = 3,
+
+    [EnumMember]
+    Other = 4
+}
+
+[DataContract]
+public class PreAdvideModel
+{
+    [DataMember]
+    public long ContainerId { get; set; }
+
+    [DataMember]
+    public PreAdviceDepartureReasonModel DepartureReason { get; set; }
+}
