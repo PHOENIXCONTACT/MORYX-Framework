fd9717971ede3774e6599d4bed944ea529fb1170
Robert Siemens <148870436+robertsmns@users.noreply.github.com>
2026-08-12 09:28:32 +0200

    Fix duplicate DisplayName ArgumentException (#1072)
    * Add warning log and safe overwrite handling for duplicate DisplayNames to prevent dictionary key conflicts.
    * Resolve warnings in code
    * Use identifier as key; add DisplayName to CellPropertySettings; unify creation via helper
    * Clean up and improve FactoryMonitor Converter
    
    Co-authored-by: Matho Camara <mcamara@phoenixcontact.com>
    Co-authored-by: robertsmns

--- src/Moryx.FactoryMonitor.Endpoints/Models/CellPropertySettings.cs
+++ src/Moryx.FactoryMonitor.Endpoints/Models/CellPropertySettings.cs
@@ -19,4 +19,6 @@ public class CellPropertySettings
     public virtual bool IsDisplayed { get; set; }
     [DataMember]
     public virtual object CurrentValue { get; set; }
+    [DataMember]
+    public string DisplayName { get; set; }
 }
