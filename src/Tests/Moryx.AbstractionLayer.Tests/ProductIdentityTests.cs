a9a4c29b3f1656ffc534e96209d91989d99ddba1
Dennis Beuchler <dbeuchler@phoenixcontact.com>
2026-08-18 10:52:45 +0200

    Merged ProductIdentityTests

--- src/Tests/Moryx.AbstractionLayer.Tests/ProductIdentityTests.cs
+++ src/Tests/Moryx.AbstractionLayer.Tests/ProductIdentityTests.cs
@@ -5,11 +5,42 @@ using System;
 using Moryx.AbstractionLayer.Products;
 using NUnit.Framework;
 
-namespace Moryx.Products.IntegrationTests;
+namespace Moryx.AbstractionLayer.Tests;
 
 [TestFixture]
 public class ProductIdentityTests
 {
+    private const string Identifier = "4564654";
+    private const short Revision = 5;
+
+    private ProductIdentity _identity;
+
+    [SetUp]
+    public void Setup()
+    {
+        _identity = new ProductIdentity(Identifier, Revision);
+    }
+
+    /// <summary>
+    /// Tests the ToString Method, will be used in serveral components which will using only the identifier string
+    /// Example: Communication with SAP, don't change the result of the method!
+    /// </summary>
+    [Test]
+    public void ToStringTest()
+    {
+        var expectedString = $"{Identifier}-{Revision:D2}";
+        Assert.That(_identity.ToString(), Is.EqualTo(expectedString));
+    }
+
+    /// <summary>
+    /// Set identifier should throw an exception because changing the identifier is not allowed
+    /// </summary>
+    [Test]
+    public void SetIdentifierTest()
+    {
+        Assert.Throws(typeof(InvalidOperationException), () => _identity.SetIdentifier("HelloWorld"));
+    }
+
     [TestCase("ABC-01", "ABC", 1)]
     [TestCase("ABC-DEF-01", "ABC-DEF", 1)]
     [TestCase("123-456-12", "123-456", 12)]
