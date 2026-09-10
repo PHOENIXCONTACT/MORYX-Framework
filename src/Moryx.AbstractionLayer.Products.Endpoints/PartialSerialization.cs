dbba3d8d78be1830a0795fbc10b6ced395bca5a9
Friedhelm Möbert <fmoebert@phoenixcontact.com>
2026-09-10 15:50:25 +0200

    separeted filter in partial serialization

--- src/Moryx.AbstractionLayer.Products.Endpoints/PartialSerialization.cs
+++ src/Moryx.AbstractionLayer.Products.Endpoints/PartialSerialization.cs
@@ -2,9 +2,12 @@
 // Licensed under the Apache License, Version 2.0
 
 using System.Reflection;
+using Moryx.AbstractionLayer.Recipes;
+using Moryx.AbstractionLayer.Resources;
 using Moryx.Configuration;
 using Moryx.Container;
 using Moryx.Serialization;
+using Moryx.Workplans;
 
 namespace Moryx.AbstractionLayer.Products.Endpoints;
 
@@ -43,10 +46,17 @@ public class PartialSerialization<T> : PossibleValuesSerialization
         return _serialization.GetProperties(sourceType).Where(SimpleProp);
     }
 
+    /// <summary>
+    /// List of all types where 'base'-propertyname like 'name' or 'id' should be allowed in complex properties
+    /// </summary>
+    private readonly List<Type> _typesOfNewSerialization =
+    [
+        typeof(ProductType),
+        typeof(Resource)
+    ];
+
     protected bool SimpleProp(PropertyInfo prop)
     {
-        var baseType = prop.DeclaringType;
-
         // Skip reference or domain model properties
         var type = prop.PropertyType;
         if (typeof(ProductType).IsAssignableFrom(type) ||
@@ -54,9 +64,27 @@ public class PartialSerialization<T> : PossibleValuesSerialization
             typeof(IEnumerable<ProductPartLink>).IsAssignableFrom(type))
             return false;
 
-        // Filter default properties
-        if (baseType == typeof(ProductType))
-            return false;
+        var testType = prop.DeclaringType;
+        var found = _typesOfNewSerialization.Contains(testType);
+        while (testType.BaseType != null && !found)
+        {
+            if (_typesOfNewSerialization.Contains(testType.BaseType))
+                found = true;
+            testType = testType.BaseType;
+        }
+
+        if (!found)
+        {
+            // Filter default properties
+            if (FilteredProperties.Contains(prop.Name))
+                return false;
+        }
+        else
+        {
+            // Filter default properties
+            if (_typesOfNewSerialization.Contains(prop.DeclaringType))
+                return false;
+        }
 
         return true;
     }
